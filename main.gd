a450036215c49ec42dc21b354d29e5fc0aaf0369
Sebastian Silva <sebastian@fuentelibre.org>
2024-11-10 11:47:51 -0500

    Pequeños ajustes

--- main.gd
+++ main.gd
@@ -6,16 +6,13 @@ var current_player = 0
 var timer_running = false
 
 func _on_space_pressed():
-	
 	if current_player == 2:
 		current_player = 1
 	elif current_player == 1:
 		current_player = 2
 	else:
-		current_player = 1
+		current_player = 2
 	timer_running = true
-
-
 
 func _on_reset_button_pressed():
 	player1_time = 300
@@ -24,18 +21,14 @@ func _on_reset_button_pressed():
 	timer_running = false
 	update_display()
 
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
+func _ready() -> void:	
 	get_node("Reset_Button").connect("pressed", _on_reset_button_pressed)
 	update_display()
 
 func _input(event):
 	if event.is_action_pressed("Cambio"):
-		print("hola")
 		_on_space_pressed()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -66,4 +59,4 @@ func update_display():
 		$Player1_time.add_theme_color_override("font_color", Color(1, 1, 1))
 		$Player2_time.add_theme_color_override("font_color", Color(1, 1, 1))
 	$Player1_time.text = format_time(player1_time)
-	$Player2_time.text = format_time(player2_time)
+	$Player2_time.text = format_time(player2_time) + " "
